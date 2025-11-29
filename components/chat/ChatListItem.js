07feede921b0f52cac189483d1f56c6b90dd7d5c
Bernard <https://github.com/99meetlove/ozkiss_ios_new.git>
2025-11-30 04:16:44 +0800

    Fix chat missing files

--- components/chat/ChatListItem.js
+++ components/chat/ChatListItem.js
@@ -1,13 +1,36 @@
 "use client";
 
-export default function ChatListItem({ item }) {
+export default function ChatListItem({ chat, onClick }) {
   return (
-    <div className="chat-list-item">
-      <div style={{ fontWeight: 600 }}>
-        {item?.title || "Chat Item"}
+    <div
+      className="chat-list-item"
+      onClick={onClick}
+      style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+    >
+      {/* Avatar */}
+      <img
+        src={chat.avatar || "/default-avatar.png"}
+        alt="avatar"
+        className="chat-avatar"
+        style={{
+          width: 44,
+          height: 44,
+          borderRadius: "50%",
+          objectFit: "cover",
+        }}
+      />
+
+      {/* Chat info */}
+      <div className="chat-info" style={{ flex: 1 }}>
+        <div className="chat-name">{chat.name || "Unknown User"}</div>
+        <div className="chat-message">
+          {chat.last_message || "No messages yet"}
+        </div>
       </div>
-      <div style={{ fontSize: 12, color: "#777" }}>
-        {item?.last || "No messages yet"}
+
+      {/* Time */}
+      <div className="chat-time">
+        {chat.time ? new Date(chat.time).toLocaleTimeString() : ""}
       </div>
     </div>
   );
